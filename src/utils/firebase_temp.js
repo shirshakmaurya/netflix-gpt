87d3de976c1302382b7982b01629550155dd71d4
shirshak <84394640+shirshakmaurya@users.noreply.github.com>
2024-04-13 23:32:35 +0530

    firebase.js

--- src/utils/firebase_temp.js
+++ src/utils/firebase_temp.js
@@ -11,6 +11,7 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   //not uploaded here, search in local
+  //go to firebase console, and find the in the project
 };
 
 // Initialize Firebase
